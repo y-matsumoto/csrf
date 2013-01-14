aa1764a54da15d2afb877ce4278071d871326b91
松本　雄貴 <mycb750@gmail.com>
2013-01-15 02:23:07 +0900

    Update csrf.php

--- csrf.php
+++ csrf.php
@@ -1,21 +1,15 @@
 <?php
 
 class Csrf {
-  const KEY_NAME = 'csrf';
+  	const KEY_NAME = 'csrf';
 
 	public static function generate() {
 		return $_SESSION[self::KEY_NAME] = sha1(uniqid(mt_rand(), true));
 	}
-
+	
 	public static function check($postKey) {
-		if (!isset($postKey) or !isset($_SESSION[self::KEY_NAME])) {
-			return false;
-		}
-
-		if ($_SESSION[self::KEY_NAME] != $postKey) {
-			return false;
-		}
-		
-    return true;
+		if (!isset($postKey) or !isset($_SESSION[self::KEY_NAME])) return false;
+		if ($_SESSION[self::KEY_NAME] != $postKey) return false;
+                return true;
 	}
 }
